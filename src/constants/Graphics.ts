026381f99bbf48b4849d59c4ce698f85ea067e45
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-13 05:10:58 +0000

    Create Graphics.ts
    + graphic constant

---
+++ src/constants/Graphics.ts
@@ -0,0 +1,9 @@
+export const GRAPHICS = {
+  ICONS: {
+    USER: require("../../assets/icons/user.png"),
+    SETTINGS: require("../../assets/icons/settings.png"),
+    SAVE: require("../../assets/icons/save.png"),
+    RATE_US: require("../../assets/icons/star.png"),
+    FAQ: require("../../assets/icons/faq.png"),
+  },
+};
