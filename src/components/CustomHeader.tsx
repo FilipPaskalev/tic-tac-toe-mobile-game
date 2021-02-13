9be092995a7be534ca2fcf7bb3d8042d66f99d66
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-13 04:44:31 +0000

    Update CustomHeader.tsx
    dynamically update header according of the screen

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,20 +1,48 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+
+/** Constants */
+import { SCREEN_INFO } from "../constants/ScreensInfo";
+
+/** Components */
 import LetterPress from "./LetterPress";
 import SquareButton from "./SquareButton";
 
-type TitleProps = {
+type CustomHeaderProps = {
   title: string;
 };
 
-function CustomHeader({ title }: TitleProps) {
-  return (
-    <View style={styles.container}>
-      <SquareButton />
-      <LetterPress context={title} setFontSize={36} />
-      <SquareButton />
-    </View>
-  );
+function CustomHeader({ title }: CustomHeaderProps) {
+  switch (title) {
+    case SCREEN_INFO.HOME.TITLE: {
+      return (
+        <View style={styles.container}>
+          <SquareButton navigateToScreen={SCREEN_INFO.PROFILE.NAME} />
+          <LetterPress context={title} setFontSize={36} />
+          <SquareButton navigateToScreen={SCREEN_INFO.SETTINGS.NAME} />
+        </View>
+      );
+    }
+    case SCREEN_INFO.SETTINGS.TITLE: {
+      return (
+        <View style={styles.container}>
+          <LetterPress context={title} setFontSize={36} />
+          <SquareButton navigateToScreen={SCREEN_INFO.SETTINGS.NAME} />
+        </View>
+      );
+    }
+    case SCREEN_INFO.PROFILE.TITLE: {
+      return (
+        <View style={styles.container}>
+          <LetterPress context={title} setFontSize={36} />
+          <SquareButton navigateToScreen={SCREEN_INFO.SETTINGS.NAME} />
+        </View>
+      );
+    }
+    default: {
+      return <LetterPress context={title} setFontSize={36} />;
+    }
+  }
 }
 
 const styles = StyleSheet.create({
