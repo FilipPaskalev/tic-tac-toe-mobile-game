8e93f551781c75988fcc52dce0f5614c9949bfb9
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-15 14:49:15 +0000

    Update SquareButton.tsx
    refactor
    add new props

--- src/components/SquareButton.tsx
+++ src/components/SquareButton.tsx
@@ -1,35 +1,58 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, Image, View } from "react-native";
+
+/**Hooks */
 import { useNavigation } from "@react-navigation/native";
 
+/**Constants */
+import { GRAPHICS } from "../constants/Graphics";
+import { BUTTON_SIZES } from "../constants/ButtonSizes";
+
 interface SquareButtonProps {
+  iconStyles?: object;
+  containerStyles?: object;
   navigateToScreen?: string;
-  graphic: object;
+  graphic?: object;
 }
 
-function SquareButton({ navigateToScreen, graphic }: SquareButtonProps) {
+function SquareButton({
+  navigateToScreen,
+  graphic,
+  iconStyles,
+  containerStyles,
+}: SquareButtonProps) {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
-      style={styles.container}
+      style={[
+        styles.container,
+        containerStyles && containerStyles ? containerStyles : styles.image,
+      ]}
       onPress={() =>
         navigateToScreen && navigateToScreen
           ? navigation.navigate(navigateToScreen)
           : undefined
       }
     >
-      <Image source={graphic} style={styles.icon} />
+      <Image
+        source={graphic && graphic ? graphic : GRAPHICS.ICONS.EMPTY}
+        style={[
+          styles.image,
+          iconStyles && iconStyles ? iconStyles : styles.image,
+        ]}
+      />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // flex: 1,
     // borderWidth: 1,
   },
-  icon: {
-    width: 48,
-    height: 48,
+  image: {
+    width: BUTTON_SIZES.SQUARE_BUTTON.LARGE.width,
+    height: BUTTON_SIZES.SQUARE_BUTTON.LARGE.height,
   },
 });
 
