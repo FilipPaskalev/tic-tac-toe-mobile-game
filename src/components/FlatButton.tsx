978555623f78f9002f1f43a65c9f771ddaff1ea2
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-15 14:48:44 +0000

    Update FlatButton.tsx
    add new properties

--- src/components/FlatButton.tsx
+++ src/components/FlatButton.tsx
@@ -1,31 +1,50 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, Image, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 /** Hooks */
 import { useNavigation } from "@react-navigation/native";
 
 /** Components */
 import LetterPress from "./LetterPress";
+import { BUTTON_SIZES } from "../constants/ButtonSizes";
 
 interface FlatButtonProps {
-  label: string;
+  label?: string;
+  containerStyles?: object;
+  buttonStyles?: object;
 }
 
-function FlatButton({ label }: FlatButtonProps) {
+function FlatButton({ label, containerStyles, buttonStyles }: FlatButtonProps) {
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button}>
-        <LetterPress context={label} />
+    <View style={[styles.container, containerStyles]}>
+      <TouchableOpacity style={[styles.button, buttonStyles]}>
+        <LetterPress
+          containerStyles={styles.text}
+          context={label && label ? label : ""}
+        />
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  button: {},
-  label: {},
+  container: {
+    // backgroundColor: "#2a64c0",
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#2a64c0",
+    height: BUTTON_SIZES.FLAT_BUTTON.LARGE.height,
+    justifyContent: "center",
+    alignSelf: "center",
+    borderWidth: 1,
+  },
+  text: {
+    justifyContent: "center",
+    alignSelf: "center",
+    padding: 8,
+  },
 });
 
 export default FlatButton;
