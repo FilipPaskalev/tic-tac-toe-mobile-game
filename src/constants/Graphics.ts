11e8475cb54a4e791993289ea6da7b661249a439
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-15 15:47:17 +0000

    Update Graphics.ts
    add more graphics

--- src/constants/Graphics.ts
+++ src/constants/Graphics.ts
@@ -7,6 +7,9 @@ export const GRAPHICS = {
     faq: require("../../assets/icons/faq.png"),
     empty: require("../../assets/icons/empty.png"),
     puzzle: require("../../assets/icons/moreGames.png"),
+    exit: require("../../assets/icons/exit.png"),
+    translation: require("../../assets/icons/translation.png"),
+    arrow: require("../../assets/icons/arrow.png"),
   },
   IMAGES: {
     logo: require("../../assets/images/logo.png"),
