612bf77946c52bd73c90b257b19d422efb81abe9
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-15 15:47:28 +0000

    Update HomeScreen.tsx
    refactor

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,27 +8,32 @@ import { useHeaderHeight } from "@react-navigation/stack";
 /** Constants */
 import { GRAPHICS } from "../constants/Graphics";
 
-/** Components */
+/** Custom components */
 import FlatButton from "../components/FlatButton";
 import SquareButton from "../components/SquareButton";
+import { SCREEN_INFO } from "../constants/ScreensInfo";
+import { COLORS } from "../constants/Colors";
 
 function HomeScreen() {
   return (
     <View style={[styles.screen, { paddingTop: useHeaderHeight() }]}>
       <StatusBar style="auto" />
-      <View style={styles.logoContainer}></View>
+      <Image source={GRAPHICS.IMAGES.logo} style={styles.logoContainer} />
       <View style={styles.buttonsContainer}>
-        <FlatButton label={"button 1"} buttonStyles={styles.button} />
-        <FlatButton label={"button 2"} buttonStyles={styles.button} />
-        <FlatButton label={"button 3"} buttonStyles={styles.button} />
-        <FlatButton label={"button 4"} buttonStyles={styles.button} />
+        <FlatButton label={"Player vs AI"} buttonStyles={styles.button} />
+        <FlatButton label={"Player vs Player"} buttonStyles={styles.button} />
+        <FlatButton label={"Play online"} buttonStyles={styles.button} />
+        <FlatButton label={"Score Board"} buttonStyles={styles.button} />
       </View>
       <View style={styles.footerContainer}>
         <SquareButton graphic={GRAPHICS.ICONS.rateUs} />
-        <SquareButton graphic={GRAPHICS.ICONS.faq} />
+        <SquareButton
+          graphic={GRAPHICS.ICONS.faq}
+          navigateToScreen={SCREEN_INFO.FAQ.NAME}
+        />
         <SquareButton graphic={GRAPHICS.ICONS.puzzle} />
-        <SquareButton graphic={GRAPHICS.ICONS.puzzle} />
-        <SquareButton graphic={GRAPHICS.ICONS.puzzle} />
+        <SquareButton graphic={GRAPHICS.ICONS.translation} />
+        <SquareButton graphic={GRAPHICS.ICONS.exit} />
       </View>
     </View>
   );
@@ -38,11 +43,12 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#f0b2cc",
+    backgroundColor: COLORS.BACKGROUND.APP.primary,
   },
   logoContainer: {
     borderWidth: 1,
     flex: 1,
+    width: "100%",
   },
   buttonsContainer: {
     // borderWidth: 1,
