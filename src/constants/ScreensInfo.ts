7a6a7b81c0f6857a7b8de80205021ef383867f40
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-15 23:00:42 +0000

    Update ScreensInfo.ts
    move component in constant

--- src/constants/ScreensInfo.ts
+++ src/constants/ScreensInfo.ts
@@ -1,9 +1,44 @@
+/** Screens */
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import FaqScreen from "../screens/FaqScreen";
+import OnePlayerScreen from "../screens/OnePlayerScreen";
+import TwoPlayersScreen from "../screens/TwoPlayersScreen";
+import MultiplayerScreen from "../screens/MultiplayerScreen";
+import ScoreBoardScreen from "../screens/ScoreBoardScreen";
+
 export const SCREEN_INFO = {
-  HOME: { title: "Tic Tac Toe", name: "HomeScreen" },
-  PROFILE: { title: "My Profile", name: "ProfileScreen" },
-  SETTINGS: { title: "Options", name: "SettingsScreen" },
-  FAQ: { title: "HELP / FAQ", name: "FaqScreen" },
-  ONE_PLAYER: { title: "Player vs AI", name: "OnePlayerScreen" },
-  TWO_PLAYERS: { title: "2 Players", name: "TwoPlayersScreen" },
-  MULTIPLAYER: { title: "Multiplayer", name: "MultiplayerScreen" },
+  HOME: { title: "Tic Tac Toe", name: "HomeScreen", component: HomeScreen },
+  PROFILE: {
+    title: "My Profile",
+    name: "ProfileScreen",
+    component: ProfileScreen,
+  },
+  SETTINGS: {
+    title: "Options",
+    name: "SettingsScreen",
+    component: SettingsScreen,
+  },
+  FAQ: { title: "HELP / FAQ", name: "FaqScreen", component: FaqScreen },
+  ONE_PLAYER: {
+    title: "Player vs AI",
+    name: "OnePlayerScreen",
+    component: OnePlayerScreen,
+  },
+  TWO_PLAYERS: {
+    title: "2 Players",
+    name: "TwoPlayersScreen",
+    component: TwoPlayersScreen,
+  },
+  MULTIPLAYER: {
+    title: "Multiplayer",
+    name: "MultiplayerScreen",
+    component: MultiplayerScreen,
+  },
+  SCORE_BOARD: {
+    title: "Score",
+    name: "ScoreBoardScreen",
+    component: ScoreBoardScreen,
+  },
 };
