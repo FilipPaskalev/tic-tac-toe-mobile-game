82cf25b9dd36fb9fd24bdca049b1ede183616f01
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-02-18 01:39:47 +0000

    update

--- App.tsx
+++ App.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { View } from "react-native";
+import "react-native-gesture-handler";
+import React, { FunctionComponent } from "react";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import * as Localization from "expo-localization";
+import i18n from "i18n-js";
 
-function App() {
-  return <View></View>;
-}
+/** Reducers */
+import appSettingsReducer from "./src/store/reducers/appSettings";
+import Navigation from "./src/navigation/Navigation";
+
+const rootReducer = combineReducers({
+  appSettings: appSettingsReducer,
+});
+
+const store = createStore(rootReducer);
+
+// Set the locale
+i18n.locale = Localization.locale;
+// When a value is missing from a language it'll fallback to another language with the key present.
+i18n.fallbacks = true;
+
+type Props = {};
+
+const App: FunctionComponent<Props> = () => {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+};
 
 export default App;
