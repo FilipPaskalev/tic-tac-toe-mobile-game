b88e33d4c4669ca46508e503277f1b03cb542183
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-03-01 08:00:54 +0000

    add new types

--- src/constants/PickerTypes.ts
+++ src/constants/PickerTypes.ts
@@ -2,4 +2,6 @@ export enum PICKER_TYPES {
   LANGUAGE = "language",
   DIFFICULTY_LEVEL = "difficulty level",
   THEME = "theme",
+  CHOOSE_YOUR_SIDE = "choose your side",
+  CHOOSE_FIRST_PLAYER = "choose first player",
 }
