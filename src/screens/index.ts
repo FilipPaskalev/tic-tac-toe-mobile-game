394d871f8acd133ea2794b32784154121945149a
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-03-03 10:50:23 +0000

    Create index.ts

---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+export { default as HomeScreen } from "./HomeScreen";
+export { default as ProfileScreen } from "./ProfileScreen";
+export { default as SettingsScreen } from "./SettingsScreen";
+export { default as FaqScreen } from "./FaqScreen";
+export { default as SinglePlayerSettingsScreen } from "./SinglePlayerSettingsScreen";
+export { default as SinglePlayerGameScreen } from "./SinglePlayerGameScreen";
