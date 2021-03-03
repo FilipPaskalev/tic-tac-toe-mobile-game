03b9e5d94c5a954c8985d356b99e78f09ccd4317
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-03-03 10:50:58 +0000

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from "react";
 import { View, StatusBar, StyleSheet } from "react-native";
-import i18n from "i18n-js";
 
 /** Components */
 import CustomFooter from "../components/CustomFooter";
@@ -12,14 +11,12 @@ import { GLOBAL_STYLES } from "../constants/styles";
 
 /** Enumerations */
 import { MARGIN } from "../constants/styles/Margin";
-import { I18N_KEYS } from "../constants/I18nKeys";
-import { SIZES } from "../constants/sizes";
 import { BUTTON_ACTIONS } from "../constants/ButtonActions";
 
 type Props = {};
 
 const HomeScreen: FunctionComponent<Props> = (props) => {
-  console.log("========== HomeScreen.tsx LOAD ===========");
+  console.log("LOAD ===> HomeScreen.tsx");
   return (
     <View style={styles.screen}>
       <StatusBar hidden={true} />
@@ -27,28 +24,15 @@ const HomeScreen: FunctionComponent<Props> = (props) => {
       <View style={styles.body}>
         <View style={styles.navigationContainer}>
           <FlatButton
-            style={styles.button}
-            action={BUTTON_ACTIONS.NAVIGATE_TO_SINGLE_PLAYER_SCREEN}
+            action={BUTTON_ACTIONS.NAVIGATE_TO_SINGLE_PLAYER_SETTINGS_SCREEN}
           />
-          <FlatButton
-            style={styles.button}
-            action={BUTTON_ACTIONS.NAVIGATE_TO_MULTIPLAYER_SCREEN}
-          />
-          <FlatButton
-            style={styles.button}
-            action={BUTTON_ACTIONS.NAVIGATE_TO_PLAY_ONLINE_SCREEN}
-          />
-          <FlatButton
-            style={styles.button}
-            action={BUTTON_ACTIONS.NAVIGATE_TO_SCORE_BOARD_SCREEN}
-          />
+          <FlatButton />
+          <FlatButton />
+          <FlatButton />
         </View>
         <View />
         <View style={styles.exitContainer}>
-          <FlatButton
-            style={styles.exitButton}
-            action={BUTTON_ACTIONS.EXIT_FROM_APP}
-          />
+          <FlatButton action={BUTTON_ACTIONS.EXIT_FROM_APP} />
         </View>
       </View>
       <CustomFooter />
@@ -75,14 +59,6 @@ const styles = StyleSheet.create({
     // borderWidth: 1,
     justifyContent: "center",
     marginBottom: MARGIN.M,
-  },
-  button: {
-    // borderWidth: 1,
-    ...SIZES.BTN_FLAT_M,
-    marginVertical: MARGIN.M,
-  },
-  exitButton: {
-    ...SIZES.BTN_FLAT_M,
   },
 });
 
