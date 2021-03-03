2af4423158c64e5d4959bd75f0c622f28c35ace8
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2021-03-03 10:50:11 +0000

    update

--- src/constants/ScreenNames.ts
+++ src/constants/ScreenNames.ts
@@ -1,11 +1,8 @@
 export enum SCREE_NAMES {
-  HOME = "Home",
-  PROFILE = "Profile",
-  SETTINGS = "Settings",
-  FAQ = "Faq",
-  SINGLE_PLAYER = "SinglePlayer",
-  GAME = "Game",
-  MULTIPLAYER = "Multiplayer",
-  PLAY_ONLINE = "PlayOnline",
-  SCORE_BOARD = "ScoreBoard",
+  HOME = "HomeScreen",
+  PROFILE = "ProfileScreen",
+  SETTINGS = "SettingsScreen",
+  FAQ = "FaqScreen",
+  SINGLE_PLAYERS_SETTINGS = "singlePlayerSettingsScreen",
+  SINGLE_PLAYERS_GAME = "singlePlayerGameScreen",
 }
